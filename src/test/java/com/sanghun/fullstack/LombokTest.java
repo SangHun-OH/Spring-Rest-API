fd1e2aa7213a65961c73df572c7c4ee68b2196e8
Sanghun Oh <74748487+SangHun-OH@users.noreply.github.com>
2021-05-19 00:18:24 +0900

    Update LombokTest.java

--- src/test/java/com/sanghun/fullstack/LombokTest.java
+++ src/test/java/com/sanghun/fullstack/LombokTest.java
@@ -3,6 +3,7 @@ package com.sanghun.fullstack;
 import com.sanghun.fullstack.domain.ResultVO;
 import org.junit.jupiter.api.Test;
 
+
 public class LombokTest {
     @Test
     public void ResultTest() {
